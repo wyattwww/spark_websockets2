89fea3bc3f1ad193af8cd435a9bae105f9cdc389
wyattwww <wyattwong@gmail.com>
2014-09-25 21:57:18 -0700

    no message

--- firmware/Spark-Websockets2.cpp
+++ firmware/Spark-Websockets2.cpp
@@ -27,7 +27,7 @@
 #include "spark_wiring_usbserial.h"
 #include "spark_wiring_string.h"
 
-#include "Spark-Websockets2.cpp"
+#include "Spark-Websockets2.h"
 #include <stdlib.h>
 #include <string.h>
 
